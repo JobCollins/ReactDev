369a061b2579d83efd188e9137b88f4361d719fa
JobCollins <dulo.collins@gmail.com>
2021-02-04 17:03:49 +0300

    Feature: Type Checking
    Type checking a component;s props with PropTypes

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 
 function ListContacts(props) {
@@ -22,5 +23,9 @@ function ListContacts(props) {
     )
 }
 
+ListContacts.propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact:PropTypes.func.isRequired
+}
 
 export default ListContacts
