79799adb14bacde94b63b2651cd3e474b48cf08c
JobCollins <dulo.collins@gmail.com>
2021-02-08 18:29:46 +0300

    Feature: State-Based Content Rendering
    Dynamically rendering the pages for contact list and create contact form page

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
 
 class App extends Component {
   state ={
+    screen: 'list',
     contacts : []
   }
   componentDidMount(){
@@ -24,9 +26,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts 
+        {this.state.screen === 'list' && (
+          <ListContacts 
           onDeleteContact={this.removeContact} 
           contacts={this.state.contacts}/>
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContact
+
+          />
+        )}
       </div>
     )
   }
---
+++ contacts/src/CreateContact.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+
+class CreateContact extends Component {
+    render() {
+        return(
+            <div>Create COntact.</div>
+        )
+    }
+}
+
+export default CreateContact;
