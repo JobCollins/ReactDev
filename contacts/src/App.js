ddad41f18fecabc0d98999eb0db1ce22c873b970
JobCollins <dulo.collins@gmail.com>
2021-02-27 19:09:22 +0300

    Feature: State Management
    moved the contacts variable from being a variable to being a state within App component for management. This way if any changes happen to contacts React is aware and updates the page

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,34 +1,37 @@
 import React, { Component } from "react";
 import ListContacts from './ListContacts'
 
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-]
-
-
 
 class App extends Component{
+  //Adding state to App so that App can manage it
+  state = {
+    //adding the previous contact variable to live as state
+    contacts : [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
   render(){
     return  (
       //pass the above contacts to the ListContacts component
-      <ListContacts contacts={contacts} />
+      //change to access the state within the App component
+      <ListContacts contacts={this.state.contacts} />
     )
   }
 }
--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -2,65 +2,65 @@ import React, { Component } from 'react'
 
 
 /*maps over each of the contacts in App.js*/
-// class ListContacts extends Component{
-//     render(){
-//         //access props object
-//         //console.log('Props', this.props);
-//         return (
-//             //specify the UI for contacts
-            // <ol className='contact-list'>
-            //     {/* access each item in the contacts array
-            //         key argument helps React keep track of changes
-            //         especially among items.
-            //      */}
-            //     {this.props.contacts.map((contact) => (
-            //         <li key={contact.id} className='contact-list-item'>
-            //             {/* {contact.name} */}
-            //             <div className='contact-avatar' style={{
-            //                 backgroundImage:`url(${contact.avatarURL})`
-            //             }}/>
-            //             <div className='contact-details'>
-            //                 <p>{contact.name}</p>
-            //                 <p>{contact.email}</p>
-            //             </div>
-            //             <button className='contact-remove'>
-            //                 Remove
-            //             </button>
-            //         </li>
-            //     ))}
-            // </ol>
-//         )
-//     }
-// }
+class ListContacts extends Component{
+    render(){
+        //access props object
+        //console.log('Props', this.props);
+        return (
+            //specify the UI for contacts
+            <ol className='contact-list'>
+                {/* access each item in the contacts array
+                    key argument helps React keep track of changes
+                    especially among items.
+                 */}
+                {this.props.contacts.map((contact) => (
+                    <li key={contact.id} className='contact-list-item'>
+                        {/* {contact.name} */}
+                        <div className='contact-avatar' style={{
+                            backgroundImage:`url(${contact.avatarURL})`
+                        }}/>
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.email}</p>
+                        </div>
+                        <button className='contact-remove'>
+                            Remove
+                        </button>
+                    </li>
+                ))}
+            </ol>
+        )
+    }
+}
 
 /*USING STATELESS FUNCTIONAL COMPONENT
 Use this if all that your component has is a render() method
 */
 
-function ListContacts(props) {
-    return(
-        <ol className='contact-list'>
-            {/* access each item in the contacts array
-                key argument helps React keep track of changes
-                especially among items.
-                */}
-            {props.contacts.map((contact) => (
-                <li key={contact.id} className='contact-list-item'>
-                    {/* {contact.name} */}
-                    <div className='contact-avatar' style={{
-                        backgroundImage:`url(${contact.avatarURL})`
-                    }}/>
-                    <div className='contact-details'>
-                        <p>{contact.name}</p>
-                        <p>{contact.email}</p>
-                    </div>
-                    <button className='contact-remove'>
-                        Remove
-                    </button>
-                </li>
-            ))}
-        </ol>
-    )
-}
+// function ListContacts(props) {
+//     return(
+//         <ol className='contact-list'>
+//             {/* access each item in the contacts array
+//                 key argument helps React keep track of changes
+//                 especially among items.
+//                 */}
+//             {props.contacts.map((contact) => (
+//                 <li key={contact.id} className='contact-list-item'>
+//                     {/* {contact.name} */}
+//                     <div className='contact-avatar' style={{
+//                         backgroundImage:`url(${contact.avatarURL})`
+//                     }}/>
+//                     <div className='contact-details'>
+//                         <p>{contact.name}</p>
+//                         <p>{contact.email}</p>
+//                     </div>
+//                     <button className='contact-remove'>
+//                         Remove
+//                     </button>
+//                 </li>
+//             ))}
+//         </ol>
+//     )
+// }
 
 export default ListContacts
