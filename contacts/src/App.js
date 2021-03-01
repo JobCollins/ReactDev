4a821035c717f070ecb668c2e7b44be47af9f8ad
JobCollins <dulo.collins@gmail.com>
2021-03-01 20:17:02 +0300

    Feature: API Calls to Fetch Data
    using componentDidMount() method ap can make network requests outside the render() method. App is using api to fetch remote contacts data and then update app state with new data

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,31 +1,21 @@
 import React, { Component } from "react";
 import ListContacts from './ListContacts'
-
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component{
   //Adding state to App so that App can manage it
   state = {
     //adding the previous contact variable to live as state
-    contacts : [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts : []
+  }
+
+  //CREATE LIFECYCLE EVENT TO MAKE NETWORK REQUESTS
+  componentDidMount(){
+    // make API request to send our contacts data 
+    ContactsAPI.getAll().then((contacts) => {
+      //update our state with new data
+      this.setState({ contacts : contacts })
+    })
   }
   //function responsible for updating current state
   removeContact = (contact) => {
