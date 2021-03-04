9f37e16900b0b561505fd209590201862407603e
JobCollins <dulo.collins@gmail.com>
2021-03-04 13:13:06 +0300

    Feature: Redux DevTools
    added a condition to check for redux dev tools on browser for easy app state montioring and app debuggging instead of console logging everytime

--- meals/src/index.js
+++ meals/src/index.js
@@ -7,7 +7,11 @@ import { createStore } from 'redux'
 import reducer from './reducers'
 
 //create store
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  //invoking redux devtools if it exists on the browser. For debugging
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+  )
 
 // console.log(store);
 
